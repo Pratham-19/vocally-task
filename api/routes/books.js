b17b918675670f949bc83bdd2cde7ce7b2dd4289
Pratham <prathamsinghal2003@gmail.com>
2023-01-18 16:42:38 +0530

    :rocket: Added books api

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,9 +1,50 @@
-const express= require('express');
+const express = require("express");
 const router = express.Router();
-const Book = require('../models/books');
+const Book = require("../models/books");
 
-router.get('/',(req,res)=>{
+router.get("/", (req, res) => {
+  Book.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json({
+        count: docs.length,
+        books: docs,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
+router.post("/addBook", (req, res) => {
+  console.log(req.body);
+  Book.find({ name: req.body.name })
+    .exec()
+    .then((user) => {
+      if (user.length) {
+        return res.status(409).json({
+          message: "Book exists",
+        });
+      }
+      const book = new Book({
+        name: req.body.name,
+        genre: req.body.genre,
+        author: req.body.author,
+      });
+      book
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(201).json({
+            message: "Book added",
+            // book: result,
+          });
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3003;
-require('dotenv').config()
-const bookRouter = require('./api/routes/books');
+require("dotenv").config();
+const bookRouter = require("./api/routes/books");
 
-app.use('/books',bookRouter);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-try{
-    mongoose.connect(`mongodb+srv://root:${process.env.MONGODB_ATLAS_KEY}@testcluster.d8tzrhx.mongodb.net/?retryWrites=true&w=majority`,{useNewUrlParser:true});
-    console.log('Connected to MongoDB!');
+app.use("/books", bookRouter);
+
+try {
+  mongoose.connect(
+    `mongodb+srv://root:${process.env.MONGODB_ATLAS_KEY}@testcluster.d8tzrhx.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  );
+  console.log("Connected to MongoDB!");
+} catch (err) {
+  console.log(err);
 }
-catch(err){
-    console.log(err);
-}
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
